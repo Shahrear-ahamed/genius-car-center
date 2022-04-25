bbc0232bd4ab7098fa36f1dd3b961271bb93edd8
Shahrear-ahamed <dev.shahrear@gmail.com>
2022-04-25 22:47:46 +0600

    edit some code

--- src/Pages/Order/Order.js
+++ src/Pages/Order/Order.js
@@ -1,9 +1,23 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
 const Order = () => {
+  const [user] = useAuthState(auth);
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    const url = `http://localhost:5000/order?email=${user.email}`;
+    const findOrers = async () => {
+      const getOrder = await axios.get(url);
+      setOrders(getOrder.data);
+    };
+    findOrers();
+  }, [user]);
   return (
     <div>
-      <h2>Order page</h2>
+      <h2>Your total orders {orders.length}</h2>
     </div>
   );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -57,6 +57,9 @@ const Header = () => {
                   <Nav.Link as={Link} to="manage">
                     Manage
                   </Nav.Link>
+                  <Nav.Link as={Link} to="orders">
+                    Order
+                  </Nav.Link>
                 </>
               )}
               {user ? (
